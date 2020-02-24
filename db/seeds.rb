29b89c6c90df07c82a6666f94da918d4dd561675
Laurent Barbezieux <laurent.barbezieux@gmail.com>
2020-02-24 18:22:10 +0100

    ongoing seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,44 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+p "reset database"
+
+Report.destroy_all
+User.destroy_all
+Company.destroy_all
+
+
+p "create company"
+
+company = Company.create!(name: "Strawberry")
+
+p "create orga"
+
+orga1 = Orga.new(name: "Supply Chain")
+orga1.company = company
+orga1.save
+
+orga1 = Orga.new(name: "Manufacturing")
+orga1.company = company
+orga1.save
+
+orga1 = Orga.new(name: "Marketing")
+orga1.company = company
+orga1.save
+
+orga1 = Orga.new(name: "Sales")
+orga1.company = company
+orga1.save
+
+orga1 = Orga.new(name: "Finance")
+orga1.company = company
+orga1.save
+
+p "create emission_modules"
+
+combfossiles = EmissionModule.create!(name: "Combustibles fossiles")
+comborganiques = EmissionModule.create!(name: "Combustibles organiques")
+changement_affectation_sols = EmissionModule.create!(name: "Changement affectation des sols")
+deforestation = EmissionModule.create!(name: "Deforestation & reforestation")
+
