13ac9be0a7e8f15c04dd2a1efac80d14c43e25e6
Laurent Barbezieux <laurent.barbezieux@gmail.com>
2020-02-25 12:39:28 +0100

    WIP seed

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,2 +1,5 @@
 class Company < ApplicationRecord
+  has_many :users
+  has_many :reports
+  has_many :orgas
 end
--- app/models/report_scope.rb
+++ app/models/report_scope.rb
@@ -1,3 +1,6 @@
 class ReportScope < ApplicationRecord
   belongs_to :report
+  belongs_to :emission_module
+  has_many :report_scope_orgas
+  has_many :answers
 end
