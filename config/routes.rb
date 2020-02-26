c3a72e76884fda8d5edb819df1646e4f6f7cd9ab
Laurent Barbezieux <laurent.barbezieux@gmail.com>
2020-02-26 18:42:14 +0100

    updated show navbar homepage

--- config/routes.rb
+++ config/routes.rb
@@ -3,19 +3,18 @@ Rails.application.routes.draw do
   root to: 'pages#home'
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-    
-  resources :reports, except: :new do
+
+  resources :reports do
     resources :report_scopes, except: :destroy
     resources :report_scope_orgas
     # Route custom pour affichage du graphique
     get "results", to: "reports#result"
   end
-  
+
   # Route pour update du status
   resources :report_scope_orgas, only: [] do
     get "send_report", to: "report_scope_orgas#send_report"
   end
-  
+
   resources :report_scopes, only: :destroy
-  
 end
