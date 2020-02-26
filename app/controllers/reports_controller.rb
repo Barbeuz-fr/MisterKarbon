207153c6782e2e04105486fd6205fe596c73c0ea
julien laloum <djulesdev@gmail.com>
2020-02-26 17:22:24 +0100

    no change

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -109,7 +109,7 @@ class ReportsController < ApplicationController
   private
 
   def report_params
-    params.require(:report).permit(:name, :year, :company_id, :id, :user_id,  :photo)
+    params.require(:report).permit(:name, :year, :company_id, :id, :user_id, :photo)
   end
 
 
