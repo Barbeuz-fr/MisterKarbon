5b14fd1f8ee38899a8d27b45b1a1e355b2c64bf5
Thomas Eude <tmmeude@gmail.com>
2020-02-27 12:13:26 +0100

    correction path

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,6 @@ Rails.application.routes.draw do
   root to: 'pages#home'
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :report_scopes, only: [:create]
 
   resources :reports, except: :new do
     resources :report_scopes, except: :destroy
