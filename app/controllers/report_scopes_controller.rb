0d8df284c59a861ca0fb7d4c6d50bf8148e38815
Antoine Delannoy <antoine.delannoy@hotmail.de>
2020-03-02 19:23:27 +0100

    done please review and merge

--- app/controllers/report_scopes_controller.rb
+++ app/controllers/report_scopes_controller.rb
@@ -5,29 +5,37 @@ class ReportScopesController < ApplicationController
 
     @module_scopes = EmissionModule.all
     @report_scopes = ReportScope.all
-    @report_scope = ReportScope.new()
+    @report_scope = ReportScope.new
 
     @project_create_1_nav = true
   end
 
   def create
     @report_scope = ReportScope.new(report_scope_params)
+    @report = Report.find(params[:report_id])
+    @report_scope.report = @report
     @report_scope.save
-    redirect_to report_report_scopes_path(@report_scope.report_id, tab_name: params[:tab_name])
+    respond_to do |format|
+      format.html { redirect_to report_report_scopes_path(@report_scope.report_id, tab_name: params[:tab_name]) }
+      format.js
+    end
   end
 
   def destroy
+
+
     @report_scope = ReportScope.find(params[:id])
     @report_scope.destroy
-    redirect_to report_report_scopes_path(@report_scope.report_id)
+    @report = @report_scope.report
+    respond_to do |format|
+      format.html { redirect_to report_report_scopes_path(@report_scope.report_id) }
+      format.js
+    end
   end
 
   private
 
   def report_scope_params
-     params.require(:report_scope).permit(
-                          :emission_module_id,
-                          :report_id
-                          )
+     params.require(:report_scope).permit(:emission_module_id)
   end
 end
