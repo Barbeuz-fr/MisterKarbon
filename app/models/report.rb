65909c8eee35ec69b0185ecb6c952bb7ed18e159
julien laloum <djulesdev@gmail.com>
2020-03-04 12:35:10 +0100

    improve cards

--- app/models/report.rb
+++ app/models/report.rb
@@ -6,4 +6,14 @@ class Report < ApplicationRecord
   has_many :orgas, through: :companies
   has_many :report_scope_orga_users, through: :users
   has_one_attached :photo
+
+
+  def report_scope_orga_users
+    users = self.report_scopes.map do |report_scope|
+      report_scope.report_scope_orgas.map do |report_scope_orga|
+        report_scope_orga.report_scope_orga_users.map {|scope_orga_user| scope_orga_user.user}
+      end
+    end
+    users.flatten.map { |user| user  }
+  end
 end
