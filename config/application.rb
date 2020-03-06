7e445500b8c84298652fc5eb5e7b8b0c98aa0b73
Thomas Eude <tmmeude@gmail.com>
2020-03-06 11:07:31 +0100

    last de last

--- config/application.rb
+++ config/application.rb
@@ -18,6 +18,11 @@ module MisterKarbon
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
+    config.to_prepare do
+      # Configure single controller layout
+      Devise::SessionsController.layout "home"
+
+    end
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
