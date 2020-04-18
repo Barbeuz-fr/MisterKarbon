5cbfab2d11bc77c16f76d5be722b18d7f1461b01
Laurent Barbezieux <laurent.barbezieux@gmail.com>
2020-04-18 14:33:52 +0200

    debug

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,7 +77,7 @@ Rails.application.configure do
     :port                 => 587,
     :user_name            => ENV["KARBONCHAIN_GMAIL_USERNAME"],
     :password             => ENV["KARBONCHAIN_GMAIL_PASSWORD"],
-    :authentication       => "plain"
+    :authentication       => "plain",
     :enable_starttls_auto => true
   }
 
