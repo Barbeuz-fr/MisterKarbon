04a10503275294cd0f206bb9c97a81a1b469513f
Laurent Barbezieux <laurent.barbezieux@gmail.com>
2020-05-08 13:34:14 +0200

    debug GAnalytics

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,7 +8,6 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
-
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
@@ -29,10 +28,14 @@ import { activeQueryDashboard} from '../plugins/tab-dashboard';
 
 initAjaxScroll();
 
+
+
 document.addEventListener('turbolinks:load', function () {
   if (document.getElementById('mybarChart')) {
     stackedBar();
   }
+  // gtag('config', 'UA-165936086-1');
+
   initActiveNavItem();
   initToolTip();
   initSubmit();
