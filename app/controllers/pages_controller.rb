0b45e7797bbb1e9a952e12d4645b2b535ff1c116
Alex Huyn <huyn.alex@gmail.com>
2020-05-10 19:16:52 +0200

    about page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,11 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home ]
+  skip_before_action :authenticate_user!, only: [ :home, :about ]
 
   def home
+    render :layout => 'home'
+  end
+
+  def about
     render :layout => 'home'
   end
 end
