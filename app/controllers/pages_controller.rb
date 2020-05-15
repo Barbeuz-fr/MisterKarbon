a08ca42cbc94c04e709d7e209cddb735ebd832ee
Alex Huyn <huyn.alex@gmail.com>
2020-05-15 17:14:58 +0200

    mentions légales

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,11 +1,15 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home, :about ]
+  skip_before_action :authenticate_user!, only: [ :home, :about, :mentions_legales ]
 
   def home
     render :layout => 'home'
   end
 
   def about
+    render :layout => 'home'
+  end
+
+  def mentions_legales
     render :layout => 'home'
   end
 end
